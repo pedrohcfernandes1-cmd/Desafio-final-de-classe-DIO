e41569d33aab1fbcc281c37cfeed867631884fd5
Pedro Carvalho <pedro.hcfernandes1@gmail.com>
2026-06-01 17:34:56 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        if (this.tipo === "mago") {
+            ataque = "magia";
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada";
+        } else if (this.tipo === "monge") {
+            ataque = "artes marciais";
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken";
+        } else {
+            ataque = "um ataque desconhecido";
+        }
+
+        console.log(`O ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+const heroi2 = new Heroi("Arthur", 30, "guerreiro");
+const heroi3 = new Heroi("Lee", 28, "monge");
+const heroi4 = new Heroi("Hanzo", 35, "ninja");
+
+heroi1.atacar();
+heroi2.atacar();
+heroi3.atacar();
+heroi4.atacar();
